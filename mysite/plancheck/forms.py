3963db4d838117e5b065987253276f2c87926435
GWJ <pylonsarenice@gmail.com>
2017-08-13 21:07:36 +0100

    fiddling

--- mysite/plancheck/forms.py
+++ mysite/plancheck/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class PatientForm(forms.Form):
     bool = forms.BooleanField(label='Boolean', required=False)
     patientid = forms.CharField(label='Patient ID', max_length=100)
+
+class PlanForm(forms.Form):
+    selectedplan = forms.ChoiceField(label="Select a plan" )
+
+
+
--- mysite/plancheck/views.py
+++ mysite/plancheck/views.py
@@ -1,28 +1,70 @@
 from django.shortcuts import redirect, render, reverse
 
-from .forms import PatientForm
+from .forms import PatientForm, PlanForm
+
 
 def index(request):
     # if this is a POST request we need to process the form data
-        if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
-            form = PatientForm(request.POST)
-            # check whether it's valid:
-            if form.is_valid():
-                patientid = form.cleaned_data['patientid']
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PatientForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            patientid = form.cleaned_data['patientid']
 
-                # process the data in form.cleaned_data as required
-                # ...
-                # redirect to a new URL:
-                #return redirect('view_plans', args=[patientid])
-                return view_plans(request, patientid)
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # return redirect('view_plans', args=[patientid])
+            request.method = 'GET'
+            print(request)
+            return view_plans(request, patientid)
 
-        # if a GET (or any other method) we'll create a blank form
-        else:
-            form = PatientForm()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = PatientForm()
 
-        return render(request, 'plancheck/index.html', {'form': form})
+    return render(request, 'plancheck/index.html', {'form': form})
+
 
 def view_plans(request, patientid):
+    print('here')
 
-    return render(request, 'plancheck/view_plans.html', {'patientid': patientid})
+    # ---- Set up some dummy plans
+    plans = []
+    for plan in patientid:
+        plans.append(plan)
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PlanForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            selectedplan = form.cleaned_data['selectedplan']
+
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # return redirect('view_plans', args=[patientid])
+
+            # ---- This is daft and doesn't work
+            request.method = 'GET'
+            print(request)
+            return index(request)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+
+        form = PlanForm()
+
+        # ---- Not sure why this is needed, should prob used init function in PlanForm
+        form.fields['selectedplan'].choices = []
+
+
+        choices = []
+        for idx in range(len(plans)):
+            form.fields['selectedplan'].choices.append((idx, plans[idx]))
+
+
+    return render(request, 'plancheck/view_plans.html', {'form': form, 'patientid': patientid, }, )
